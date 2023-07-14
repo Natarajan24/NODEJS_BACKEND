cdcf4223bd4fa3d7833a8de9e9d0420d8ef5e5ed
natu80 <natarajnatarajc99@gmail.com>
2023-07-14 11:29:23 +0530

    jj

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 const express = require("express");
 const app = express();
-const exhbs = require("express-handlebars");
-const jwt = require("jsonwebtoken");
-const dbo = require("./dbaccess/model_details_access");
 const loginRouter = require("./routes/login_page");
-
-app.engine(
-  "hbs",
-  exhbs.engine({ layoutsDir: "views/", defaultLayout: "main", extname: "hbs" })
-);
-app.set("view engine", "hbs");
-app.set("views", "views");
 
 app.use("/login", loginRouter);
 
