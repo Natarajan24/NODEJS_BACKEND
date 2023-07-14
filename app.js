251c205e9a3b2ab6e5b25024749e248e03dd1cfb
natu80 <natarajnatarajc99@gmail.com>
2023-07-14 12:40:15 +0530

    changes

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const app = express();
-const loginRouter = require("./routes/login_page");
+const dbo = require("./main/dbaccess/model_details_access");
+const { ObjectId } = require("mongodb");
 
-app.use("/login", loginRouter);
+app.get("/login/:id", async (req, res) => {
+  pipeline = [
+    {
+      $match: {
+        _id: new ObjectId(req.params.id),
+      },
+    },
+  ];
+
+  const employee = await dbo.aggregateModel(pipeline, "user_login");
+  res.send(employee);
+});
+
+app.post("/login", async (req, res) => {
+  let datas = await dbo.insertModel(
+    { name: "ajith", email: "ddcsdcds" },
+    "user_login"
+  );
+  res.send(datas);
+});
 
 app.listen(8000, () => {});
